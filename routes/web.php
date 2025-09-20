acc80872fe32b79aeabe38aef734c738a9e489a2
ahmed <ahmedzid@aol.com>
2025-09-20 18:35:14 +0100

    corrected some of the role access to the app

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,4 @@
 <?php
-
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ProjectController;
@@ -25,20 +24,19 @@ Route::middleware(['auth', 'verified'])->group(function () {
         ->name('dashboard');
 
     Route::resource('project', ProjectController::class);
+
+    // Corrected "My Tasks" route for technicians
     Route::get('/task/my-tasks', [TaskController::class, 'myTasks'])
+        ->middleware(TechnicienMiddleware::class) // Apply the middleware here
         ->name('task.myTasks');
+
     Route::resource('task', TaskController::class);
     Route::resource('user', UserController::class);
-});
-// Admin Routes
+
+    // Admin Routes
     Route::middleware([AdminMiddleware::class])->group(function () {
         Route::resource('user', UserController::class);
         Route::resource('equipement', EquipementController::class);
-    });
-
-    // Technicien Routes
-    Route::middleware([TechnicienMiddleware::class])->group(function () {
-        Route::get('/task/my-tasks', [TaskController::class, 'myTasks'])->name('task.myTasks');
     });
 
     // Technical Manager Routes
@@ -52,21 +50,18 @@ Route::middleware(['auth', 'verified'])->group(function () {
     // Project Manager Routes
     Route::middleware([ProjectManagerMiddleware::class])->group(function () {
         Route::get('/projects/{project}/tasks', [ProjectController::class, 'tasks'])->name('project.tasks');
-        Route::get('/task/my-tasks', [TaskController::class, 'myTasks'])->name('task.myTasks');
     });
 
-
-    Route::resource('project', ProjectController::class);
-    Route::resource('task', TaskController::class);
-Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-Route::resource('equipement', EquipementController::class);
-Route::get('/equipement-assignment', [EquipementAssignmentController::class, 'index'])->name('equipement.assignment.index');
+
+    Route::resource('equipement', EquipementController::class);
+    Route::get('/equipement-assignment', [EquipementAssignmentController::class, 'index'])->name('equipement.assignment.index');
     Route::post('/equipement-assignment/assign', [EquipementAssignmentController::class, 'assign'])->name('equipement.assignment.assign');
-Route::get('/notifications', [App\Http\Controllers\NotificationController::class, 'index'])
-    ->middleware(['auth', 'verified'])
-    ->name('notifications.index');
+
+    Route::get('/notifications', [App\Http\Controllers\NotificationController::class, 'index'])
+        ->name('notifications.index');
+});
+
 require __DIR__ . '/auth.php';
